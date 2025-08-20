2b427a852111c8d77a1a85764e513b3210d05c9c
Atte <atte.hakkila@gmail.com>
2025-08-20 11:50:12 +0000

    valmis

--- Ohjelmisto1/module2/teht2e.py
+++ Ohjelmisto1/module2/teht2e.py
@@ -1,1 +1,10 @@
-
+leiviska = int(input("Anna leiviskat. "))
+x = leiviska * 20
+naula = int(input("Anna naulat. "))
+y = x + naula
+z = y * 32
+luoti = int(input("Anna luodit. "))
+a = luoti + z
+b = a * 13.3
+c = b / 1000
+print(f"Massa nykymittojen mukaan: {c:.2f}kg")
