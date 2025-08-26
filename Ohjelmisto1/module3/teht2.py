b43af5209b3282ad81ee49d96d81533c837273d7
Atte <atte.hakkila@gmail.com>
2025-08-26 10:51:04 +0000

    valmis

--- Ohjelmisto1/module3/teht2.py
+++ Ohjelmisto1/module3/teht2.py
@@ -1,1 +1,11 @@
-
+x = input("Anna hyttiluokkasi: ")
+if x=="LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif x=="A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif x=="B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif x=="C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Väärin!")
