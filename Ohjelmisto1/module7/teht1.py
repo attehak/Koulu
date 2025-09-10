f6855f4f30b45bda1d3f3323203ef08daa245324
Atte <atte.hakkila@gmail.com>
2025-09-10 14:35:37 +0300

    Update teht1.py

--- Ohjelmisto1/module7/teht1.py
+++ Ohjelmisto1/module7/teht1.py
@@ -1,1 +1,12 @@
+x = (
+    ("talvi", (11, 12, 1)),
+    ("kevät", (2, 3, 4)),
+    ("kesä", (5, 6, 7)),
+    ("syksy", (8, 9, 10))
+)
+z = int(input("anna kuukausi (1-12): "))
 
+for y, a in x:
+    if z in a:
+        print(f"{z} on {y}.")
+        break
