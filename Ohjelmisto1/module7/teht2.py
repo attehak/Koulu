30e81f639e098729e8e4ba8ab609bc26aa125b6b
Atte <atte.hakkila@gmail.com>
2025-09-10 15:37:44 +0300

    teht2 valmis

--- Ohjelmisto1/module7/teht2.py
+++ Ohjelmisto1/module7/teht2.py
@@ -1,1 +1,19 @@
+x = set()
+z = input("Anna nimi: ")
+x.add(z)
+print("Uusi nimi")
 
+while True:
+    z = input("Anna nimi: ")
+    if z == "":
+        break
+
+    if z in x:
+        print("Aiemmin syötetty nimi")
+
+    else:
+        print("Uusi nimi")
+        x.add(z)
+
+for i in x:
+    print(i)
