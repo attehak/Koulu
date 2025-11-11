f477c01a28fdd7b542d7cd170adc8e300f762e8d
Atte <atte.hakkila@gmail.com>
2025-11-11 13:48:26 +0200

    Create 1.js

---
+++ Ohjelmisto1/JavaScript/module3/1/1.js
@@ -0,0 +1,10 @@
+const list = document.querySelector("#target")
+
+const html = 
+`
+<li> First item</li>
+<li> Second item</li>
+<li> Third item</li>
+`
+list.classList.add("my-list")
+list.innerHTML = html
